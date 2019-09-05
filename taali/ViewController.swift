c3902b7bf74b455a2a0267f017449c067b79c589
mdayunus <mdayunus@gmail.com>
2019-09-05 19:28:50 +0530

    Initial Commit

---
+++ taali/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taali
+//
+//  Created by Mohammad Yunus on 05/09/19.
+//  Copyright © 2019 taptap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
